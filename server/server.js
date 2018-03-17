5909582106d0c652d42c226511c5b80886eb4ba8
przemyslaw.pedziwiatr@gmail.com <przemyslaw.pedziwiatr@gmail.com>
2018-03-17 21:25:33 +0100

    Added todo by id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const {ObjectID} = require('mongodb');
 var express = require('express');
 var bodyParser = require('body-parser');
 
@@ -29,6 +30,24 @@ app.get('/todos', (req, res) => {
         res.status(400).send(e);
     });
 })
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if(!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+     Todo.findById(id).then((todo) => {
+         if(!todo) {
+            res.status(404).send();
+         }
+         res.send({todo});
+     }, (e) => {
+         res.status(400).send();
+     });
+
+});
 
 app.listen(3000, () => {
     console.log('Started on port 3000');
