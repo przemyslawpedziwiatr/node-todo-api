0d60f8a8f33f0a270b47b8d16853a3ea5bb363bc
przemyslaw.pedziwiatr@gmail.com <przemyslaw.pedziwiatr@gmail.com>
2018-03-18 10:42:40 +0100

    Setup app for heroku

--- server/server.js
+++ server/server.js
@@ -7,8 +7,9 @@ var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
+const port = process.env.PORT || 3000;
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
     var todo = new Todo({
@@ -21,7 +22,7 @@ app.post('/todos', (req, res) => {
     }, (e) => {
         res.status(400).send(e);
     });
-})
+});
 
 app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
@@ -29,7 +30,7 @@ app.get('/todos', (req, res) => {
     }, (e) => {
         res.status(400).send(e);
     });
-})
+});
 
 app.get('/todos/:id', (req, res) => {
     var id = req.params.id;
@@ -49,8 +50,8 @@ app.get('/todos/:id', (req, res) => {
 
 });
 
-app.listen(3000, () => {
-    console.log('Started on port 3000');
+app.listen(port, () => {
+    console.log(`Started on port ${port}`);
 });
 
 module.exports = {app};
