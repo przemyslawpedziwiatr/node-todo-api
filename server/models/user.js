8763bceeafcfa12fbd3c342da7521bd81c103b50
przemyslaw.pedziwiatr@gmail.com <przemyslaw.pedziwiatr@gmail.com>
2018-03-18 22:29:09 +0100

    Added /users/login /users/me POST login route

--- server/models/user.js
+++ server/models/user.js
@@ -58,6 +58,28 @@ UserSchema.methods.toJSON = function () {
     return _.pick(userObject, ['_id', 'email']);
 }
 
+UserSchema.statics.findByCredentials = function (email, password) {
+    var User = this;
+
+    return User.findOne({
+        email
+    }).then(user => {
+        if (!user) {
+            return Promise().reject();
+        }
+
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password, user.password, (err, res) => {
+                if (res) {
+                    resolve(user);
+                } else {
+                    reject(err);
+                }
+            });
+        });
+    });
+}
+
 UserSchema.statics.findByToken = function (token) {
     var User = this;
     var decoded;
--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const {
 } = require('mongodb');
 const express = require('express');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcryptjs');
 
 const {
     mongoose
@@ -133,6 +134,18 @@ app.post('/users', (req, res) => {
 
 app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
+});
+
+app.post('/users/login', (req, res) => {
+    var body = _.pick(req.body, ['email','password']);
+    
+    User.findByCredentials(body.email, body.password).then((user) => {
+        return user.generateAuthToken().then((token) => {
+            res.header('x-auth',token).send(user);
+        });
+    }).catch((e) => {
+        res.status(400).send();
+    });
 });
 
 app.listen(port, () => {
