29b03d27f46806e42d39db4391e4684760f5034a
przemyslaw.pedziwiatr@gmail.com <przemyslaw.pedziwiatr@gmail.com>
2018-03-18 12:11:25 +0100

    Added PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,16 +1,17 @@
+const _ = require('lodash');
 const {
     ObjectID
 } = require('mongodb');
-var express = require('express');
-var bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-var {
+const {
     mongoose
 } = require('./db/mongoose.js');
-var {
+const {
     Todo
 } = require('./models/todo');
-var {
+const {
     User
 } = require('./models/user');
 
@@ -80,6 +81,37 @@ app.delete(`/todos/:id`, (req, res) => {
     });
     
 });
+
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);
+
+    if(!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {
+        $set: body
+    }, { 
+        new: true
+    }).then((todo) => {
+        if(!todo) {
+            return res.status(404).send();
+        }
+
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
+    });
+    
+})
 
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
